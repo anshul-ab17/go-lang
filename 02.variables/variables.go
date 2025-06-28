4dd344f87f173759a939b0d965c84b916688d676
Anshul Bharat <anshul.ab17x@gmail.com>
2025-06-29 02:54:50 +0530

    uint8 float64 variables.

--- 02.variables/variables.go
+++ 02.variables/variables.go
@@ -7,8 +7,10 @@ import "fmt"
 //  --String boolean int float64 uint8 int8 int16 int32 int64 float32 float64 complex.
 
 //    var variableName variableType = value
+ 
 
-
+const token1 = "1234567890abcdef"
+// const token1 :="1234567890abcdef"  cannot use := in global scope, must be inside a function /  method
 
 
 func main(){
@@ -32,6 +34,21 @@ func main(){
 	var float1 float64 =259.4554454543
 	fmt.Println("\nFloat1:", float1)
 	fmt.Printf("variable type: %T", float1)
+
+	// defining multiple variables
+	var firstName, lastName string = "John", "Doe"	
+	fmt.Println(lastName,firstName)
+
+	// no var keyword
+	lastName2 := "Smith"
+	fmt.Println(lastName2)
+
+
+	// implict type inference - automatically defines the type based on the value
+	var city = "New York"		
+	fmt.Println(city)
+
+	fmt.Println(token1)
 
 
 
